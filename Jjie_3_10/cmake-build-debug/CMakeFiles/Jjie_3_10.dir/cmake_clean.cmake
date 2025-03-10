756479c06b09a61aa47ac02c404ba7b5abc4bd57
Jjie0707 <1778836683@qq.com>
2025-03-10 17:11:04 +0800

    测试

---
+++ Jjie_3_10/cmake-build-debug/CMakeFiles/Jjie_3_10.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Jjie_3_10.dir/main.c.o"
+  "CMakeFiles/Jjie_3_10.dir/main.c.o.d"
+  "Jjie_3_10"
+  "Jjie_3_10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Jjie_3_10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
