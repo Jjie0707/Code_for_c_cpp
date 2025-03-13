5843eea86e3a5c267e9818a0dd94259a53fbd1c8
Jjie0707 <1778836683@qq.com>
2025-03-13 23:47:08 +0800

    修正

--- Jjie_3_13/main.c
+++ Jjie_3_13/main.c
@@ -1,58 +1,36 @@
-#include <stdio.h>
+#include<stdio.h>
 int trans(char s) {
     switch (s) {
-        case 'a':
-            return 0;
-        case 'e':
-            return 1;
-        case 'i':
-            return 2;
-        case 'o':
-            return 3;
-        case 'u':
-            return 4;
-        default:
-            return -1;
+        case 'a':return 0;
+        case 'e':return 1;
+        case 'i':return 2;
+        case 'o':return 3;
+        case 'u':return 4;
+        default:return -1;
     }
 }
 #include <string.h>
-// 至少 出现k个辅音 每个元音至少出现一次
-// 差分(k-(k+1))得到恰好出现k个
 long long _countOfSubstrings(char* word, int k) {
     long long count = 0;
-    int arr[5] = {0};//   每种元音出现的次数
-    int kinds = 0;//   出现的元音的种类
-    int consonant = 0;//  辅音出现的次数
-
+    int arr[5] = {0};  //   每种元音出现的次数
+    int kinds = 0;     //   出现的元音的种类
+    int consonant = 0; //  辅音出现的次数
     int length = strlen(word);
-
-    int left = 0, right = 5 + k - 1;
-
-    for (int i = 0; i <= right; i++) {
-        if (trans(word[i]) >= 0) {
-            if (arr[trans(word[i])] == 0) {
+    int left = 0, right = 0;
+    while (right < length) {
+        if (trans(word[right]) >= 0) {
+            if (arr[trans(word[right])] == 0) {
                 kinds += 1;
             }
-            arr[trans(word[i])] += 1;
+            arr[trans(word[right])] += 1;
         } else {
             consonant += 1;
-        } //直接检查第一个窗口是否满足
-    }
-    while (right < length) {
-        // 不满足扩大右窗口
-
-            if (trans(word[right]) >= 0) {
-                if (arr[trans(word[right])] == 0) {
-                    kinds += 1;
-                }
-                arr[trans(word[right])] += 1;
-            } else {
-                consonant += 1;
-                right++;
-
-        // 满足收缩左窗口
-        while (kinds >= 5 && consonant >= k) {
-            count += length -right+1;
+        }
+        right++;
+        //收缩左边界
+        while(kinds>=5 && consonant>=k){
+            //当前右边界++了所以要再+1
+            count+=length-right+1;
             if (trans(word[left]) >= 0) {
                 if (arr[trans(word[left])] == 1) {
                     kinds -= 1;
@@ -69,12 +47,9 @@ long long _countOfSubstrings(char* word, int k) {
 long long countOfSubstrings(char* word, int k) {
     return _countOfSubstrings(word, k) - _countOfSubstrings(word, k + 1);
 }
-int main(void) {
-    char word[]="ieaouqqieaouqq";
-    // printf("%d ",judge('a'));
-    // printf("%d ",judge('b'));
-    printf("%d ",_countOfSubstrings(word,1));
-    printf("%d ",_countOfSubstrings(word,2));
-    // printf("Hello, World!\n");
+int main() {
+    char word[]="aeioup";
+    printf("%d ",countOfSubstrings(word,0));
+    printf("%d ",countOfSubstrings(word,1));
     return 0;
 }
