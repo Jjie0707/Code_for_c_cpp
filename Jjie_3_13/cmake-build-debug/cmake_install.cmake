e300516e565710827dfdd4cd08a7500a9aa2606a
Jjie0707 <1778836683@qq.com>
2025-03-13 22:57:13 +0800

    刷题

--- Jjie_3_13/cmake-build-debug/cmake_install.cmake
+++ Jjie_3_13/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/ouyangjunjie/Desktop/GitHub/Code_for_c_cpp/Jjie_3_13
+# Install script for directory: /Users/jjie/Desktop/GitHub/Code_for_c_cpp/Jjie_3_13
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/ouyangjunjie/Desktop/GitHub/Code_for_c_cpp/Jjie_3_13/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/jjie/Desktop/GitHub/Code_for_c_cpp/Jjie_3_13/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
