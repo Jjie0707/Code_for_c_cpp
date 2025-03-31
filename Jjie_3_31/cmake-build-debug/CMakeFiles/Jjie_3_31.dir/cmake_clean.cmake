d552c410a79a9fd4acc7e10201aec248bba1e8a3
Jjie0707 <1778836683@qq.com>
2025-03-31 22:35:31 +0800

    exercise

---
+++ Jjie_3_31/cmake-build-debug/CMakeFiles/Jjie_3_31.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Jjie_3_31.dir/main.c.o"
+  "CMakeFiles/Jjie_3_31.dir/main.c.o.d"
+  "Jjie_3_31"
+  "Jjie_3_31.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Jjie_3_31.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
